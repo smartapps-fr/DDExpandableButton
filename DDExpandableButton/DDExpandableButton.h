9046184de675799655274c571b5c82d2ba826eff
Damien Debin <ddebin@yahoo.com>
2012-10-02 17:11:44 +0200

    License clarification.

--- DDExpandableButton/DDExpandableButton.h
+++ DDExpandableButton/DDExpandableButton.h
@@ -1,27 +1,14 @@
 //
 //  DDExpandableButton.h
-//  Created by ddebin.
 //  https://github.com/ddebin/DDExpandableButton
 //
 
-/*
- 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
- */
+#ifndef __IPHONE_3_0
+#warning "This project uses features only available in iOS SDK 3.0 and later."
+#endif
 
 #import <UIKit/UIKit.h>
+
 
 #define DDView UIView <DDExpandableButtonViewSource>
 
